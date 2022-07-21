e0aca13c3252809eeff49869c65a71b8fedde18f
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-07-21 23:12:12 +0530

    Update __main__.py

--- Yuriko/__main__.py
+++ Yuriko/__main__.py
@@ -233,7 +233,7 @@ def start(update: Update, context: CallbackContext):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_photo(
-                    photo=random.choice{PHOTO},
+                    photo = f"{random.choice(PHOTO)}",
                     caption=PM_START_TEXT.format(
                     escape_markdown(first_name),
                     escape_markdown(uptime),
