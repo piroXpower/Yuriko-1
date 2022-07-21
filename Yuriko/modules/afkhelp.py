1e11c3ca6eecb5ea0d3a3d27e7c29da8f1112d3c
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-07-22 01:33:19 +0530

    Create afkhelp.py

---
+++ Yuriko/modules/afkhelp.py
@@ -0,0 +1,12 @@
+__mod_name__ = "𝙰ғᴋ"
+
+__help__ = """
+
+**AFK:**
+
+`When marked as AFK, any mentions will be replied to with a message stating that you're not available!`
+
+✗ /afk - `<reason> Mark yourself as AFK.`
+
+brb - `<reason> Same as the afk command, but not a command.` 
+"""
