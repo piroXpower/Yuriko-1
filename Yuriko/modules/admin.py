8554c4ad33d2194b3b10ff726d7ea3c6a4f22a1f
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-07-22 00:28:41 +0530

    Update admin.py

--- Yuriko/modules/admin.py
+++ Yuriko/modules/admin.py
@@ -924,8 +924,6 @@ __help__ = """
 
 ✗ /admins - `list of admins in the chat`
 
-✗ /pinned - `to get the current pinned message.`
-
 *The Following Commands are Admins only:* 
 
 ✗ /pin - `silently pins the message replied to - add 'loud' or 'notify' to give notifs to users`
@@ -936,25 +934,13 @@ __help__ = """
 
 ✗ /promote - `promotes the user replied to`
 
-✗ /fullpromote - `promotes the user replied to with full rights`
-
 ✗ /demote - `demotes the user replied to`
-
-✗ /title - `<title here> sets a custom title for an admin that the bot promoted`
 
 ✗ /admincache - `force refresh the admins list`
 
 ✗ /del - `deletes the message you replied to`
 
 ✗ /purge - `deletes all messages between this and the replied to message.`
-
-✗ /purge - `<integer X> deletes the replied message, and X messages following it if replied to a message.`
-
-✗ /setgtitle - `<text> set group title`
-
-✗ /setgpic - `reply to an image to set as group photo`
-
-✗ /setdesc - `Set group description`
 
 ✗ /setsticker - `Set group sticker`
 
