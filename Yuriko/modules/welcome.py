95ad8d50482be9bd5582598c5dff05b4e7607d55
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-07-22 01:02:58 +0530

    Update welcome.py

--- Yuriko/modules/welcome.py
+++ Yuriko/modules/welcome.py
@@ -1238,24 +1238,17 @@ __help__ = """
 *Admins Command Only:*
 
 ✗ /welcome - `<on/off> enable/disable welcome messages.`
-
 ✗ /welcome - `noformat shows current welcome settings, without the formatting - useful to recycle your welcome messages!`
 
 ✗ /goodbye - `same usage and args as `/welcome`.`
-
 ✗ /setwelcome - `<sometext> set a custom welcome message. If used replying to media, uses that media.`
-
 ✗ /setgoodbye - `<sometext> set a custom goodbye message. If used replying to media, uses that media.`
-
 ✗ /resetwelcome - `reset to the default welcome message.`
-
 ✗ /resetgoodbye - `reset to the default goodbye message.`
-
 ✗ /cleanwelcome - `<on/off> On new member, try to delete the previous welcome message to avoid spamming the chat.`
 
 
 ✗ /welcomemutehelp - `gives information about welcome mutes.`
-
 ✗ /cleanservice - `<on/off deletes telegrams welcome/left service messages.`
 
  *Example:*
