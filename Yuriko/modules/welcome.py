3904d9792b676efaa4422577f41c43737c3b06ba
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-07-22 00:55:33 +0530

    Update welcome.py

--- Yuriko/modules/welcome.py
+++ Yuriko/modules/welcome.py
@@ -1239,8 +1239,6 @@ __help__ = """
 
 ✗ /welcome - `<on/off> enable/disable welcome messages.`
 
-✗ /welcome - `shows current welcome settings.`
-
 ✗ /welcome - `noformat shows current welcome settings, without the formatting - useful to recycle your welcome messages!`
 
 ✗ /goodbye - `same usage and args as `/welcome`.`
