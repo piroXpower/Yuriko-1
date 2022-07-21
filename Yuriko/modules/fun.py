7e198403d7e40bf8dfd5767e5fed8385ac653a55
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-07-22 00:51:41 +0530

    Update fun.py

--- Yuriko/modules/fun.py
+++ Yuriko/modules/fun.py
@@ -314,26 +314,17 @@ def weebify(update: Update, context: CallbackContext):
 
 __help__ = """
 ✗ /runs - `reply a random string from an array of replies`
-
 ✗ /slap - `slap a user, or get slapped if not a reply`
-
 ✗ /shrug - `get shrug XD`
 ✗ /table - `get flip/unflip :v`
-
 ✗ /decide - `Randomly answers yes/no/maybe`
-
 ✗ /toss - `Tosses A coin`
 ✗ /bluetext - `check urself :V`
 ✗ /roll - `Roll a dice`
-
 ✗ /rlg - `Join ears,nose,mouth and create an emo ;-;`
-
 ✗ /shout - `<keyword> write anything you want to give loud shout`
-
 ✗ /weebify - `<text> returns a weebified text`
-
 ✗ /sanitize - `always use this before /pat or any contact`
-
 ✗ /pat - `pats a user, or get patted`
 ✗ /8ball - `predicts using 8ball method`
 
@@ -349,12 +340,8 @@ __help__ = """
 
 ✗ /weebify - `<text>: weebify your text!`
 ✗ /bubble - `<text>: bubble your text!`
-
 ✗ /fbubble - `<text>: bubble-filled your text!`
-
-
 ✗ /fsquare - `<text>: square-filled your text!`
-
 ✗ /square - `<text>: square your text!`
 ✗ /blue - `<text>: bluify your text!`
 ✗ /latin - `<text>: latinify your text!`
